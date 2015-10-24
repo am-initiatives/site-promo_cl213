e5ef8dd3b2b14aa10e76cd0c477bdd782df72474
Adrien Debord <adrien.debord@gmail.com>
2015-10-24 23:30:17 +0200

    ajout fonctionnalité : ajout de pages d'erreurs

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="alert-box alert">
+	<h1 class="text-center">Erreur 403 : Accès Interdit !</h1>
+</div>
+@endsection
+
+@section('scripts')
+@parent
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="alert-box">
+	<h1 class="text-center">Erreur 404 : Page non trouvée</h1>
+</div>
+@endsection
+
+@section('scripts')
+@parent
+@endsection
