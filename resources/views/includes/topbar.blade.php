92d53d1aeb9b1152af9f8a0253db5b9f4e95181e
Adrien Debord <adrien.debord@gmail.com>
2015-11-06 09:21:19 +0100

    fix : icone event

--- resources/views/includes/topbar.blade.php
+++ resources/views/includes/topbar.blade.php
@@ -29,7 +29,7 @@
 			<ul class="right">
 				<li><a href="{{ route('tools.map') }}"><i class="fa fa-map-o"></i> Carte</a></li>
 				<li><a href="{{ route('users.index') }}"><i class="fa fa-users"></i> Annuaire</a></li>
-				<li><a href="{{ route('event.index') }}"><i class="fa fa-users"></i> Evènements</a></li>
+				<li><a href="{{ route('event.index') }}"><i class="fa fa-calendar-check-o"></i> Evènements</a></li>
 				<li class="has-dropdown">
 					<a href="#"><i class="fa fa-btc"></i> Buqueur</a>
 					<ul class="dropdown">
