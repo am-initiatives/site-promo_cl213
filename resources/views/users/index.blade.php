66d3c267e2c42b819c1ae6be6e36188196dfab3a
Corentin GITTON <corentin.gitton@gmail.com>
2015-11-09 10:27:26 +0100

    + : Un peu de zal'oeil sur la page annuaire

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,37 +4,40 @@
 
 <div class="panel">
 @unless(count($users))
-	<p>Aucun utilisateur.</p>
+	<p class="text-center">Aucun utilisateur.</p>
 @else
-	<table class="small-12">
-		<caption>Liste des utilisateurs</caption>
-		<thead>
-			<th></th>
-			<th>Buque</th>
-			<th>Nom, Prénom</th>
-			<th>e-mail</th>
-			<th>Tel</th>
-		</thead>
-		<tbody>
-			@foreach($users as $user)
-			<tr>
-				<td>
+	<div class="row">
+		@foreach($users as $user)
+		<div class="columns small-12 medium-6">
+			<div class="vignette clearfix">
+				<div class="picture left">
+					<img src="{{ $user->getPictureLink() }}">
+				</div>
+				<div class="information left">
+					@if(in_array($user->nickname, [null, '']))
+					<a href="{{ route('users.show', $user->id) }}">{{ $user->last_name }} {{ $user->first_name }}</a>
+					@else
+					<a href="{{ route('users.show', $user->id) }}">{{ $user->nickname }} <span style="font-size: small">({{ $user->last_name }} {{ $user->first_name }})</span></a>
+					@endif
+					<br/>
+					{{ $user->email }}<br/>
+					{{ $user->phone }}<br/>
+				</div>
+				<div class="actions right text-right">
+					{!! Html::userIcons($user) !!}
 					@if(Auth::user()->isAllowed("log_as"))
+						@if(!in_array(Html::userIcons($user), [null, '']))
+						<br/>
+						@endif
 						<a href="{{route('auth.log_as',$user->id)}}">
 							<i class="fa fa-sign-in"></i>
 						</a>
 					@endif
-					{!! Html::userIcons($user) !!}
-				</td>
-				<td>{{ $user->nickname }}</td>
-				<td>{{ $user->last_name }} {{ $user->first_name }}</td>
-				<td>{{ $user->email }}</td>
-				<td>{{ $user->phone }}</td>
-				<td></td>
-			</tr>
-			@endforeach
-		</tbody>
-	</table>
+				</div>
+			</div>
+		</div>
+		@endforeach
+	</div>
 @endunless
 </div>
 
