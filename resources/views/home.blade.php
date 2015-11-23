818330e48e8b5da010e3aa29b39f269510077121
Adrien Debord <adrien.debord@gmail.com>
2015-11-23 17:19:50 +0100

    + : masqué les champs "à payer" et "à recevoir" quand le montant est de zéro

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,12 @@
 				<a href="{{ route('users.account.show', $user->id) }}">
 					<div class="panel" style="text-align: center;">
 						Solde : {!! Html::solde($user->getBalance()) !!}<br>
+						@if($user->debits()->pending()->count()!=0)
 						A payer : {!! Html::solde(-$user->debits()->pending()->sum("amount")) !!}<br>
+						@endif
+						@if($user->credits()->pending()->count()!=0)
 						A Recevoir : {!! Html::solde($user->credits()->pending()->sum("amount")) !!}
+						@endif
 					</div>
 				</a>
 			</div>
