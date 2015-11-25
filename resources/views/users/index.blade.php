61e39974b2324bbd1eee58dfd6fd254728f3f693
Corentin GITTON <corentin.gitton@gmail.com>
2015-11-25 12:53:51 +0100

    fix : Quelques modifications au niveau de l'affichage dans la page annuaire pour éviter les retours à la ligne

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main', ['page_title' => 'Annuaire'])
 
+
 @section('content')
 
 <div>
@@ -10,6 +11,19 @@
 @else
 	<div class="row">
 		@foreach($users as $user)
+<?php
+$lines = [];
+
+if (Html::userIcons($user) != '')
+	$lines[] = Html::userIcons($user);
+
+foreach ($user->getRoles() as $role) {
+	$lines[] = '<span class="label info">' . $role . '</span>';
+}
+
+if(Auth::user()->isAllowed("log_as"))
+	$lines[] = '<a href="' . route('auth.log_as', $user->id) . '"><i class="fa fa-sign-in"></i></a>';
+ ?>
 		<div class="columns small-12 medium-6">
 			<div class="panel vignette clearfix">
 				<a href="{{ route('users.show', $user->id) }}">
@@ -22,7 +36,7 @@
 								@if(in_array($user->nickname, [null, '']))
 								{{ $user->last_name }} {{ $user->first_name }}
 								@else
-								{{ $user->nickname }}<span class="show-for-medium-up" style="font-size: small"> ({{ $user->last_name }} {{ $user->first_name }})</span>
+								{{ $user->nickname }}<span style="font-size: small;"> ({{ $user->last_name }} {{ $user->first_name }})</span>
 								@endif
 							</li>
 							<li>
@@ -31,27 +45,25 @@
 							<li class="show-for-medium-up">
 								{{ $user->email }}
 							</li>
+							@if(count($user->getRoles()))
+							<li>
+								@foreach($user->getRoles() as $role)
+								<span class="label info">{{ $role }}</span>
+								@endforeach
+							</li>
+							@endif
 						</ul>
 					</div>
+					@if(Auth::user()->isAllowed("log_as"))
+					<div class="actions">
+						<a href="{{ route('auth.log_as', $user->id) }}">
+							<i class="fa fa-sign-in"></i>
+						</a>
+					</div>
+					@endif
 				</a>
-				<div class="actions right text-right">
-					<?php
-					$lines = [];
-
-					if (Html::userIcons($user) != '')
-						$lines[] = Html::userIcons($user);
-
-					foreach ($user->getRoles() as $role) {
-						$lines[] = '<span class="label info">' . $role . '</span>';
-					}
-
-					if(Auth::user()->isAllowed("log_as"))
-						$lines[] = '<a href="' . route('auth.log_as', $user->id) . '"><i class="fa fa-sign-in"></i></a>';
-					 ?>
-
-					{!! implode('<br/>', $lines) !!}
-				</div>
 			</div>
+			
 		</div>
 		@endforeach
 	</div>
