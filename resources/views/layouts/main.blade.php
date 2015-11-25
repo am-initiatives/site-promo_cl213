6c35c47b398028a1cd78b70693de15ce0c5a584e
Adrien Debord <adrien.debord@gmail.com>
2015-11-25 19:34:28 +0100

    fix : tri des soldes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -34,10 +34,42 @@
 @parent
 <script src="{{ URL::to('tablesorter-2.0/jquery.tablesorter.min.js') }}"></script>
 <script type="text/javascript">
+
+	// mettre un tag pour le tri oblige à surcharcher l'extraction de texte ce qui fout la merde
+	// pour le tri des chiffres du type <strong>100</strong> qui sera alors interprété comme chaine 
+	//  $.tablesorter.addParser({ 
+	// 	// set a unique id 
+	// 	id: 'byTag', 
+	// 	is: function(s) { 
+	// 		var data = s.match(/sortby="([^"]+)"/);
+	// 		return data ? true : false; 
+	// 	}, 
+	// 	format: function(s) { 
+	// 		// format your data for normalization 
+	// 		var res =  s.match(/sortby="([^"]+)"/)[1]; 
+	// 		return res;
+	// 	}
+	// }); 
+	var solde = /(-?[0-9]{1,2}\.[0-9]{2})\s?€/;
+	 $.tablesorter.addParser({ 
+		// set a unique id 
+		id: 'solde', 
+		is: function(s) { 
+			var data = s.match(solde);
+			return data ? true : false; 
+		}, 
+		format: function(s) { 
+			// format your data for normalization 
+			var res =  s.match(solde)[1]; 
+			return res;
+		},
+		type:"numeric"
+	}); 
 	$(document).ready(function() 
-	    { 
-	        $(".sortable").tablesorter(); 
-	    } 
+		{ 
+			// $(".sortable").tablesorter( {textExtraction: function(node){return node.innerHTML;}}); 
+			$(".sortable").tablesorter(); 
+		} 
 	); 
 </script>
 @endsection
